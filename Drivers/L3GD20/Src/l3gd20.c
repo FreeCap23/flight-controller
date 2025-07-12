c64c1690bac02dcbb484ad2d798c8ca725458aae
Dionisie Stratulat <dionisiestratulat@gmail.com>
2025-07-12 10:34:51 +0300

    Add initial L3GD20 Drivers
    Right now these files only contain the defines. Functions will be added
    in a later commit.

---
+++ Drivers/L3GD20/Src/l3gd20.c
@@ -0,0 +1,11 @@
+/**
+  ******************************************************************************
+  * @file    l3gd20.c
+  * @author  Dionisie Stratulat
+  * @brief   This file provides a set of functions needed to manage the L3GD20,
+  *          ST MEMS motion sensor, 3-axis digital output gyroscope.
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "l3gd20.h"
